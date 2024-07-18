c1ba20eefa92dbb49e1ab335ff5aedaf317165ef
Mr-Mudassar <mudassarmuhammad776@gmail.com>
2024-07-18 19:33:58 +0500

    Project Base setup with basic routing and Tailwind Css installation done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from "react";
-// import MiningScreen from "./components/MiningScreen";
-import PlayScreen from "./components/PlayScreen";
+import "./App.css";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+
+// pages imports
+import Home from "./pages/home";
+import About from "./pages/about";
+import NotFound from "./pages/404";
+import Faqs from "./pages/faqs";
+
 function App() {
   return (
-    <>
-      <PlayScreen />
+    <div>
+      <Router>
+        <Routes>
+          {/* 404 (not found) */}
+          <Route path="*" element={<NotFound />} />
 
-      {/* Changes by Mudassar  for testing the branches and Pull requests*/}
-
-      {/* <MiningScreen /> */}
-    </>
+          {/* Pages */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faqs" element={<Faqs />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
---
+++ src/pages/home/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-xl text-red-600 font-bold text-center'>Home</div>
+  )
+}
+
+export default Home
