0454f9ccad60d20b85c7e32d3c219c0923ba82bd
MuhammadSaranKhalid <muhammadsarankhalid@gmail.com>
2024-08-19 17:10:11 +0500

    All balance available coin image changed

--- src/pages/mining/index.js
+++ src/pages/mining/index.js
@@ -4,9 +4,16 @@ import React, { useState, useEffect } from "react";
 import DetailsButton from "../../components/detailsButton";
 
 // images import
-import { ButtonCoin, ButtonLion, CountingCoin, TapCoin, ShockIcon, MainBackground, Boost } from '../../assests/svgs/index'
+import {
+  ButtonCoin,
+  ButtonLion,
+  CountingCoin,
+  TapCoin,
+  ShockIcon,
+  MainBackground,
+  Boost,
+} from "../../assests/svgs/index";
 import { Link } from "react-router-dom";
-
 
 const levelThresholds = [
   { name: "Bronze", maxLevel: 10 },
@@ -54,7 +61,9 @@ const Mining = () => {
       if (newBalance >= coinsPerLevel) {
         setCurrentLevel((prevLevel) => {
           const newLevel = prevLevel + 1;
-          const newTier = levelThresholds.find((threshold) => newLevel <= threshold.maxLevel)?.name || "Bronze";
+          const newTier =
+            levelThresholds.find((threshold) => newLevel <= threshold.maxLevel)
+              ?.name || "Bronze";
           setCurrentTier(newTier);
           return newLevel;
         });
@@ -93,7 +102,7 @@ const Mining = () => {
           </div>
         </button>
         <button className="bg-gradient-to-b from-[#99E23B] to-[#547C21] px-2 py-1 rounded-lg">
-          <Link to={'/settings'} className="flex items-center gap-x-2">
+          <Link to={"/settings"} className="flex items-center gap-x-2">
             <img src={ButtonLion} />
             <p className="text-center text-xs text-white">Choose Exchange</p>
           </Link>
@@ -103,22 +112,48 @@ const Mining = () => {
       <div className="bg-gradient-to-b from-[#74561d] via-[#303c20] to-[#303c20] py-6 rounded-t-3xl shadow-lg shadow-[#9b8255]">
         <div className="flex justify-center space-x-4">
           {/* 3 details button */}
-          <DetailsButton className="text-center shadow-lg bg-gray-100 p-3  rounded-md" heading={"Earn per Tap"} IsIcon={true} value={`+${perTapValue}`} />
-          <DetailsButton className="text-center shadow-lg bg-gray-100 p-4 rounded-md" heading={"Profit per hour"} IsIcon={true} value={"0"} />
-          <DetailsButton className="text-center shadow-lg bg-gray-100 p-4 rounded-md" heading={"Daily Task"} IsIcon={true} value={"0"} />
+          <DetailsButton
+            className="text-center shadow-lg bg-gray-100 p-3  rounded-md"
+            heading={"Earn per Tap"}
+            IsIcon={true}
+            value={`+${perTapValue}`}
+          />
+          <DetailsButton
+            className="text-center shadow-lg bg-gray-100 p-4 rounded-md"
+            heading={"Profit per hour"}
+            IsIcon={true}
+            value={"0"}
+          />
+          <DetailsButton
+            className="text-center shadow-lg bg-gray-100 p-4 rounded-md"
+            heading={"Daily Task"}
+            IsIcon={true}
+            value={"0"}
+          />
         </div>
 
         {/* Coin counting */}
-        <div className="flex items-center">
-          <img className="w-40" src={CountingCoin} alt="leon-counting-coin"/>
-          <p className="text-white text-3xl mr-4 font-semibold">{availableBalance}</p>
+        <div className="flex text-center justify-center mt-4">
+          <span className="flex items-center">
+            <img className="w-28" src={CountingCoin} alt="leon-counting-coin" />
+            <p className="text-white text-3xl mr-4 font-semibold">
+              {availableBalance}
+            </p>
+          </span>
         </div>
 
         {/* Progress bar */}
         <div className="px-4">
           <div className="flex justify-between mb-1">
             <p className="text-white text-xs">Level {currentTier}</p>
-            <p className="text-white text-xs">Level {currentLevel}/{levelThresholds.find((threshold) => threshold.name === currentTier)?.maxLevel}</p>
+            <p className="text-white text-xs">
+              Level {currentLevel}/
+              {
+                levelThresholds.find(
+                  (threshold) => threshold.name === currentTier
+                )?.maxLevel
+              }
+            </p>
           </div>
           <div className="bg-black relative h-4 w-full rounded-2xl">
             <div
@@ -132,8 +167,10 @@ const Mining = () => {
         <div className="text-center">
           <button
             className="transform transition-transform duration-0 scale-95 active:scale-100 1 focus:outline-none"
-            onClick={handleButtonClick} disabled={!canTap}>
-            <img src={TapCoin} alt="tab-coin"/>
+            onClick={handleButtonClick}
+            disabled={!canTap}
+          >
+            <img src={TapCoin} alt="tab-coin" />
           </button>
         </div>
 
@@ -148,8 +185,6 @@ const Mining = () => {
             <p className="text-center text-white text-xl">Boost</p>
           </div>
         </div>
-
-
       </div>
     </div>
   );
